85eed61755b233c7a8f20a9602922a0535d0e2e7
SanthoshS20 <santhoshsekar97@gmail.com>
2021-07-06 18:21:40 +0530

    Added Other Language Folders

---
+++ C/ProblemID_1672.c
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/richest-customer-wealth/submissions/
+
+/*
+
+Problem Description 
+
+You are given an m x n integer grid accounts where accounts[i][j] is the amount of money the ith customer has in the jth bank. 
+Return the wealth that the richest customer has.
+
+A customer's wealth is the amount of money they have in all their bank accounts. The richest customer is the customer that has the maximum wealth.
+
+
+Time Complexity    Space Complexity
+    O(N^2)             O(1)
+
+*/
+
