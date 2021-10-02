1658a642ed9d7262656332fdb154f4b4b07d6476
Ayan Banerjee <33069606+Ayan1089@users.noreply.github.com>
2021-10-02 12:44:11 +0530

    Added solution for problem 1672
    Added the java solution for problem 1672 for Hactoberfest.

--- Java/ProblemID_1672.java
+++ Java/ProblemID_1672.java
@@ -15,3 +15,18 @@ Time Complexity    Space Complexity
 
 */
 
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int max = -9999;
+        for(int i=0;i<accounts.length;i++)
+        {
+            int sum = 0;
+            for(int j=0;j<accounts[i].length;j++)
+                sum+=accounts[i][j];
+            if(sum>max){
+                max = sum;
+            }
+        }
+        return max;
+    }
+}
