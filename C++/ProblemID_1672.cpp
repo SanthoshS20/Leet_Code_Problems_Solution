5f53f04de8d3528995b872a922485a0cd4262a76
diwakarshukla <diwakarshukla@fico.com>
2021-10-02 14:07:27 +0530

    solution added for problem ID 1672

--- C++/ProblemID_1672.cpp
+++ C++/ProblemID_1672.cpp
@@ -15,3 +15,15 @@ Time Complexity    Space Complexity
 
 */
 
+class Solution { 
+    public int maximumWealth(int[][] accounts) {
+        int max = 0; 
+        for (int i = 0; i < accounts.length; i++) {
+            int total = 0;
+            for (int j = 0; j < accounts[i].length; j++) {
+                total += accounts[i][j];
+            }
+            max = Math.max(max, total);
+        }
+        return max;
+    } 
