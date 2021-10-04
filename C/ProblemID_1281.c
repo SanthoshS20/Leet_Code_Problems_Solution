a1c1e5cf18db9c27cffc2312210cd2905b82a188
Nimesha Madhushani <90630567+kanimesha99@users.noreply.github.com>
2021-10-03 23:35:16 -0700

    corrected the program to get a output

--- C/ProblemID_1281.c
+++ C/ProblemID_1281.c
@@ -1,19 +1,14 @@
-/*
-
-Link -   https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
-
-
-Problem Description : 
-
-Given an integer number n, return the difference between the product of its digits and the sum of its digits.
-
-*/
-
-//     Time Complexity     Space Complexity
-//        O(log n)              O(1)
-
-
-int subtractProductAndSum(int n){
+#include<stdio.h>
+int subtractProductAndSum(int n);
+void main()
+{
+    int num;
+    printf("Enter the number:");
+    scanf("%d",&num);
+    printf("The Difference between product and sum of %d is %d",num,subtractProductAndSum(num));
+}
+int subtractProductAndSum(int n)
+{
     int product = 1;
     int sum = 0;
     while(n!=0)
