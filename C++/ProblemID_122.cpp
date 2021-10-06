a257819610b1507bb28f433b5218212e93bbb935
Gopal-Dahale <dahalegopal27@gmail.com>
2021-10-06 17:01:18 +0530

    Added Solution for Problem ID 122

---
+++ C++/ProblemID_122.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+/*
+
+Problem Description 
+
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+Time Complexity    Space Complexity
+    O(N)             O(1)
+
+*/
+
+class Solution
+{
+public:
+  int maxProfit(vector<int> &prices)
+  {
+    int res = 0;
+    int n = prices.size();
+    for (int i = 0; i < n - 1; i++)
+    {
+      if (prices[i + 1] > prices[i])
+        res += prices[i + 1] - prices[i];
+    }
+    return res;
+  }
+};
